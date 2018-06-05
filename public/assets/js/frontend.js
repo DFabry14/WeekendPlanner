a9c819b9416dc4c13c48e6e3fd8091910be9e96b
Ben Swolo <rachelkindacodes@gmail.com>
2018-06-05 13:42:31 -0500

    jq

---
+++ public/assets/js/frontend.js
@@ -0,0 +1,16 @@
+
+// ***** CALENDAR ***** //
+var dayStartBtn = $("#day-start");
+var dayEndBtn = $("#day-end");
+var datePicker = $(".datepicker");
+
+dayStartBtn.on("click", function() {
+  // Calendar shows up to default day
+  console.log("clicked!");
+  datePicker.datepicker("show");
+  // Choose day
+  // Pull value from day
+  // Show calendar for day-end
+  // Chose day
+  // Pull value from day
+});
