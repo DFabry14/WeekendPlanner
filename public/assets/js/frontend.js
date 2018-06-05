ce14db640ba7ec5696e16020d544318060aa6f79
Ben Swolo <rachelkindacodes@gmail.com>
2018-06-05 14:39:54 -0500

    frond end

--- public/assets/js/frontend.js
+++ public/assets/js/frontend.js
@@ -1,16 +1,25 @@
 
-// ***** CALENDAR ***** //
-var dayStartBtn = $("#day-start");
-var dayEndBtn = $("#day-end");
-var datePicker = $(".datepicker");
+$(document).ready(function() {
 
-dayStartBtn.on("click", function() {
-  // Calendar shows up to default day
-  console.log("clicked!");
-  datePicker.datepicker("show");
-  // Choose day
-  // Pull value from day
-  // Show calendar for day-end
-  // Chose day
-  // Pull value from day
+  console.log("connected");
+  // ***** CALENDAR ***** //
+  var dayStartBtn = $("#day-start");
+  var dayEndBtn = $("#day-end");
+  var datePicker1 = $(".datepicker-1");
+  var datePicker2 = $(".datepicker-2");
+
+  datePicker1.datepicker();
+  datePicker1.hide();
+
+  dayStartBtn.on("click", function () {
+    // Calendar shows up to default day
+    console.log("clicked!");
+    datePicker1.slideDown("slow");
+    datePicker1.datepicker("show");
+    // Choose day
+    // Pull value from day
+    // Show calendar for day-end
+    // Chose day
+    // Pull value from day
+  });
 });
