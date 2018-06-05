e470ccb648491be9116b20beb3c38f3caaaccb10
James McGinn <jamangutan@gmail.com>
2018-06-05 10:19:34 -0500

    model setup

--- models/events.js
+++ models/events.js
@@ -1,6 +1,10 @@
 module.exports = function(sequelize, DataTypes){
     var UserEvent = sequelize.define("UserEvent", {
-        
+        eventName: DataTypes.STRING,
+        eventDate: DataTypes.INTEGER,
+        eventTime: DataTypes.INTEGER,
+        eventURL: DataTypes.STRING,
+        eventCity: DataTypes.STRING
     });
     return UserEvent
 };
