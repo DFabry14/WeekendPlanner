c133dd150a4a9f5739687ab87397e8ed776b60f3
James McGinn <jamangutan@gmail.com>
2018-06-06 11:38:45 -0500

    reformatted model and seeded dummy data to DB

---
+++ data/seeds.sql
@@ -0,0 +1,4 @@
+use qpdouh0o5ptkcv4o;
+insert into UserEvents (eventName, eventDate, eventTime, eventURL, eventCity) VALUES ( 'Test',04222018,1200,'ww.url.com','city');
+insert into UserEvents (eventName, eventDate, eventTime, eventURL, eventCity) VALUES ( 'Test1',05222018,1200,'ww.url1.com','city1');
+insert into Users (userName) VALUES ( 'Testy McTesterson');
