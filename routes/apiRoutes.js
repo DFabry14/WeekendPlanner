04177bef9077c5d91a4cca9f80ffc62257be031a
Dan Fabry <dfabry14@gmail.com>
2018-06-07 11:15:59 -0500

    changed search results to handlebars, coded the dates to always be upcoming weekend, dynamically created 10 event boxes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,39 @@
 const db = require("../models");
+var axios = require("axios");
 
 module.exports = function (app) {
 
-    app.get('/api/events', (req, res) => db.UserEvent
-        .findAll({})
-        .then(data => res.json(data)));
+    // app.get('/api/events', (req, res) => db.UserEvent
+    //     .findAll({})
+    //     .then(data => res.json(data)));
 
-    app.post('/api/events', (req, res) => db.UserEvent
-        .create(req.body)
-        .then(data => res.json(data)));
+    // app.post('/api/events', (req, res) => db.UserEvent
+    //     .create(req.body)
+    //     .then(data => res.json(data)));
 
-    app.delete('/api/events/:id', (req, res) => db.UserEvent
-        .destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(data => res.json(data)));
+    // app.delete('/api/events/:id', (req, res) => db.UserEvent
+    //     .destroy({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     })
+    //     .then(data => res.json(data)));
 
-    app.put('/api/events/:id', (req, res) => db.UserEvent
-        .update(req.body, {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(data => res.json(data)));
+    // app.put('/api/events/:id', (req, res) => db.UserEvent
+    //     .update(req.body, {
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     })
+    //     .then(data => res.json(data)));
 
 
-    app.post('/api/getevents', function(req, res){
-        // Use AXIOS to make AJAX call to EVENTFUL API
-        // Get that evenful data
-        // Pass that eventful data that was returned to
-        // res.render
-    })
+    // app.post('/api/getevents', function (req, res) {
+    //     // Use AXIOS to make AJAX call to EVENTFUL API
+    //     // Get that evenful data
+    //     // Pass that eventful data that was returned to
+    //     // res.render
+    // });
+
+    
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,48 @@
 var path = require("path");
+var axios = require("axios");
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("home");
     });
-    
+
     app.get("/links", function (req, res) {
         res.render("links");
     });
 
-    app.get("/events", function (req, res) {
-        res.render("event-display");
+    app.get("/events/:keyword", function (req, res) {
+        var keyword = req.params.keyword;
+        var friday = getNextDayOfWeek(new Date(), 5);
+        var saturday = getNextDayOfWeek(new Date(), 6);
+        var sunday = getNextDayOfWeek(new Date(), 7);
+
+        Promise.all([queryEventfulAPI(keyword, friday), queryEventfulAPI(keyword, saturday), queryEventfulAPI(keyword, sunday)])
+            .then(function (response) {
+                var fridayEvents = response[0].data.events.event;
+                var saturdayEvents = response[1].data.events.event;
+                var sundayEvents = response[2].data.events.event;
+                var eventObj = {
+                    fridayEvents, saturdayEvents, sundayEvents
+                }
+                res.render("event-display", eventObj);
+            })
     });
+    function getNextDayOfWeek(date, dayOfWeek) {
+        // got this function from here: https://codereview.stackexchange.com/questions/33527/find-next-occurring-friday-or-any-dayofweek
+        // Code to check that date and dayOfWeek are valid left as an exercise ;)
+
+        var resultDate = new Date(date.getTime());
+
+        resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+        resultDate = JSON.stringify(resultDate).replace(/[-]/g, "").slice(1, 9) + "00";
+        return resultDate + "-" + resultDate;
+        // Eventful wants the date in this format: https://api.eventful.com/docs/events/search
+    }
+
+    function queryEventfulAPI(keyword, date) {
+        var API_KEY = "4W3gpS4HDcDQZrkP";
+        var queryURL = "http://api.eventful.com/json/events/search?app_key=" + API_KEY + "&keywords=" + keyword + "&location=chicago&date=" + date + "&page_size=10";
+        var proxy = "https://cors-anywhere.herokuapp.com/";
+        return axios.get(queryURL)
+    }
 }
